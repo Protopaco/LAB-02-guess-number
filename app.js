cb17db1eba5fe3de87b775b56d709b3e53b404eb
Paul Stevens <twinkisan@hotmail.com>
2020-09-29 15:47:22 -0700

    finished intro and functionality

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
+import { gameSetup } from './gameSetup.js';
+
 // import functions and grab DOM elements
+const userDisplay = document.getElementById('display');
+const userInput = document.getElementById('user-input');
+const userButton = document.getElementById('button');
+
 
 // initialize state
 
+let gameState = 'setUp';
+const theNumber = returnRandomNumber();
+console.log(theNumber);
 // set event listeners to update state and DOM
+
+userButton.addEventListener('click', () => {
+    if (gameState === 'setUp'){
+        gameState = gameSetup(userDisplay, userButton);
+    }
+    //const theGuess = returnUserGuess();
+})
+
+function returnRandomNumber(){
+    return Math.floor(Math.random() * 20);
+}
+
+
