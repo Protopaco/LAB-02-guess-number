e637e6700efc59a55b5fa8baf6c85d2c4dd52b56
Paul Stevens <twinkisan@hotmail.com>
2020-09-29 18:03:20 -0700

    basic funcationality

--- app.js
+++ app.js
@@ -99,33 +99,63 @@
 
 
 const userDisplay = document.getElementById('display');
+const guessDisplay = document.getElementById('guessDisplay');
+
 const userInput = document.getElementById('user-input');
 const userButton = document.getElementById('button');
 
 let guessCount = 4;
 let theNumber = returnRandomNumber();
 let theGuess = '';
+let gameState = 'unWon';
+console.log(theNumber);
+
 
 userButton.addEventListener('click', () => {
-    theGuess = userInput.value;
-    console.log('theNumber: ' +theNumber);
+    guessDisplay.textContent = `you have ${guessCount} fucking guesses left`;
+
+    theGuess = Number(userInput.value);
     let response = compareNumbers(theNumber, theGuess);
-    console.log(response);
+    userInput.value = '';
+    guessCount--;
+    console.log(guessCount);
+    gameState = userResponse(userDisplay, response);
+    if(gameState === 'won') {
+        userInput.style.visibility = 'hidden';
+        userButton.style.visibility = 'hidden';
+        guessDisplay.style.visibility = 'hidden';
+        userDisplay.textContent = 'you fucking won';
+    } else if (guessCount === 0) {
+        userInput.style.visibility = 'hidden';
+        userButton.style.visibility = 'hidden';
+        guessDisplay.style.visibility = 'hidden';
+
+        userDisplay.textContent = 'you fucking lost';
+    }
 })
-
-
 
 function returnRandomNumber(){
     return Math.floor(Math.random() * 20);
 }
 
 function compareNumbers(theNumber, theGuess) {
-    console.log('theGuess: ' +theGuess)
     if (theNumber > theGuess){
         return -1;
     } else if (theNumber === theGuess) {
         return 0;
     } else {
         return 1;
+    }
+}
+
+function userResponse(userDisplay, response) {
+    if (response === -1) {
+        userDisplay.textContent = 'too low, guess higher';
+        return 'unWon';
+    } else if (response === 1) {
+        userDisplay.textContent = 'too high, guess lower';
+        return 'unWon';
+    } else {
+        return 'won';
     }
 }
