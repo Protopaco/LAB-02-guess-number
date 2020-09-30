d35aa8e1cbf24e289b1eb7e7894918fc3a8e45e3
Paul Stevens <twinkisan@hotmail.com>
2020-09-30 09:16:42 -0700

    adds guessForMe button

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const guessDisplay = document.getElementById('guessDisplay');
 const userInput = document.getElementById('user-input');
 const userButton = document.getElementById('button');
 const resetButton = document.getElementById('resetButton');
+const guessForMeButton = document.getElementById('guessForMe');
 
 let guessCount = 4;
 let theNumber = returnRandomNumber();
@@ -16,38 +17,26 @@ console.log(theNumber);
 
 userButton.addEventListener('click', () => {
     theGuess = Number(userInput.value);
-    response = compareNumbers(theNumber, theGuess);
-    guessCount--;
-    guessDisplay.textContent = `you have ${guessCount} guesses left`;
-
-    gameState = userResponse(userDisplay, response);
-    
-    if(gameState === 'won') {
-        userInput.style.visibility = 'hidden';
-        userButton.style.visibility = 'hidden';
-        guessDisplay.style.visibility = 'hidden';
-        userDisplay.textContent = 'you won';
-        resetButton.style.visibility = 'visible';
-
-    } else if (guessCount === 0) {
-        userInput.style.visibility = 'hidden';
-        userButton.style.visibility = 'hidden';
-        guessDisplay.style.visibility = 'hidden';
-        userDisplay.textContent = 'you lost';
-        resetButton.style.visibility = 'visible';
-
-    }
+    gameProcess(theGuess);
 })
+
+guessForMeButton.addEventListener('click', () => {
+    theGuess = returnRandomNumber();
+    userInput.value = theGuess.toString();
+    gameProcess(theGuess);
+});
 
 resetButton.addEventListener('click', resetGame);
 
 function resetGame() {
     guessCount = 4;
     theNumber = returnRandomNumber();
+    userInput.value = '';
     userInput.style.visibility = 'visible';
     userButton.style.visibility = 'visible';
     resetButton.style.visibility = 'hidden';
     guessDisplay.style.visibility = 'visible';
+    guessForMeButton.style.visibility = 'visible';
     userDisplay.textContent = '';
     guessDisplay.textContent = `you have ${guessCount} guesses left`;
 
@@ -67,5 +56,33 @@ function userResponse(userDisplay, response) {
         return 'unWon';
     } else {
         return 'won';
+    }
+}
+
+function gameProcess () {
+    response = compareNumbers(theNumber, theGuess);
+    guessCount--;
+    guessDisplay.textContent = `you have ${guessCount} guesses left`;
+
+    gameState = userResponse(userDisplay, response);
+    
+    if(gameState === 'won') {
+        userInput.style.visibility = 'hidden';
+        userButton.style.visibility = 'hidden';
+        guessDisplay.style.visibility = 'hidden';
+        userDisplay.textContent = 'you won';
+        resetButton.style.visibility = 'visible';
+        guessForMeButton.style.visibility = 'hidden';
+
+
+    } else if (guessCount === 0) {
+        userInput.style.visibility = 'hidden';
+        userButton.style.visibility = 'hidden';
+        guessDisplay.style.visibility = 'hidden';
+        userDisplay.textContent = 'you lost';
+        resetButton.style.visibility = 'visible';
+        guessForMeButton.style.visibility = 'hidden';
+
+
     }
 }
